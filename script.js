ba8aa38206fe2a25c167d5974aed6d375d812fcb
kevinmoore444 <kevinmoore444@gmail.com>
2023-03-05 14:00:17 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+// Challenge 1
+
+
+function pizzaoven(crust, saucetype, cheeses, toppings) {
+    var pizza = {};
+    pizza.crust = crust;
+    pizza.saucetype = saucetype;
+    pizza.cheeses = cheeses;
+    pizza.toppings = toppings;
+    return pizza;
+}
+    
+var pizza1 = pizzaoven("deep-dish", "traditional", "mozzarella", ["pepperoni", "sausage"]);
+console.log(pizza1);
+
+// Challenge 2
+
+function pizzaoven(crust, saucetype, cheeses, toppings) {
+    var pizza = {};
+    pizza.crust = crust;
+    pizza.saucetype = saucetype;
+    pizza.cheeses = cheeses;
+    pizza.toppings = toppings;
+    return pizza;
+}
+    
+var pizza1 = pizzaoven("hand-tossed", "marinara", ["mozzarella", "feta"], ["mushrooms", "olives", "onions"]);
+console.log(pizza1);
+
+
+// Challenge 3
+
+
+function pizzaoven(crust, saucetype, cheeses, toppings) {
+    var pizza = {};
+    pizza.crust = crust;
+    pizza.saucetype = saucetype;
+    pizza.cheeses = cheeses;
+    pizza.toppings = toppings;
+    return pizza;
+}
+    
+var pizza1 = pizzaoven("deep-dish", "white", ["mozzarella", "feta", "ricotta"], ["mushrooms", "olives", "onions", "peppers"]);
+console.log(pizza1);
+
+
+function pizzaoven(crust, saucetype, cheeses, toppings) {
+    var pizza = {};
+    pizza.crust = crust;
+    pizza.saucetype = saucetype;
+    pizza.cheeses = cheeses;
+    pizza.toppings = toppings;
+    return pizza;
+}
+    
+var pizza2 = pizzaoven("hand-tossed", "marinara", "mozzarella", ["olives", "onions"]);
+console.log(pizza2);
+
+
